c208647f03ed93fcd71247d2c7883f05bd81703a
Kartar Singh Jabanda <kartarsjabanda@gmail.com>
2021-01-29 15:10:42 +0000

    1.0.5

---
+++ src/main.js
@@ -0,0 +1,60 @@
+"use strict";
+exports.__esModule = true;
+var utils_1 = require("./utils");
+// import debounce from './utils/debounce'
+require("./index.css");
+var target = document.querySelector('.js-sticky-scroll-catch');
+var parent = target.parentElement;
+var targetHeight = target.offsetHeight;
+// let targetWidth: number = target.offsetWidth
+var parentHeight = parent.offsetHeight;
+// let parentWidth: number = parent.offsetWidth
+// TODO: calc width of parent - width of target
+// use that val as left: property for fixed and absolute pos
+// let leftSpace = parentWidth - targetWidth - target.getBoundingClientRect().left
+// target.style.left = `${leftSpace}px`
+var scrollDirection;
+var isCatchPos = false;
+var stickyScrollCatch = function () {
+    if (utils_1.isElmScrolledBottom(target)) {
+        utils_1.setActiveState(utils_1.STATES.SCROLL_DOWN_CATCH, target);
+        if (scrollDirection)
+            isCatchPos = true;
+    }
+    if (utils_1.isElmScrolledBottom(parent)) {
+        utils_1.setActiveState(utils_1.STATES.SCROLL_BREAK, target);
+    }
+    // handle scroll up and 'catch' so scrolls with window
+    if (!utils_1.isElmScrolledBottom(target) && scrollDirection && !isCatchPos) {
+        var YAxisSpace = parentHeight - targetHeight;
+        var topDistanceFromParent = target.getBoundingClientRect().top - parent.getBoundingClientRect().top;
+        var bottomYAxisSpace = YAxisSpace - topDistanceFromParent;
+        utils_1.setActiveState(utils_1.STATES.UPSCROLL, target);
+        target.style.bottom = bottomYAxisSpace + "px";
+        isCatchPos = true;
+    }
+    // handle scroll up and 'catch' so scrolls with window DOWNSCROLL catch
+    if (!scrollDirection && target.classList.contains('sticky-scroll-catch--scrolled-top') && !isCatchPos) {
+        var YAxisSpace = parentHeight - targetHeight;
+        var topDistanceFromParent = target.getBoundingClientRect().top - parent.getBoundingClientRect().top;
+        var bottomYAxisSpace = YAxisSpace - topDistanceFromParent;
+        utils_1.setActiveState(utils_1.STATES.DOWNSCROLL, target);
+        target.style.bottom = bottomYAxisSpace + "px";
+        isCatchPos = true;
+    }
+    if (!scrollDirection)
+        isCatchPos = false;
+    // fix to top
+    if (target.getBoundingClientRect().top > 0 && scrollDirection) {
+        utils_1.setActiveState(utils_1.STATES.SCROLL_TOP, target);
+    }
+    if (parent.getBoundingClientRect().top > 0) {
+        utils_1.setActiveState(utils_1.STATES.INITAL, target);
+    }
+    window.onscroll = function () {
+        // print "false" if direction is down and "true" if up // creds IT VLOG https://stackoverflow.com/questions/31223341/detecting-scroll-direction
+        scrollDirection = this.oldScroll > this.scrollY;
+        this.oldScroll = this.scrollY;
+    };
+};
+exports["default"] = stickyScrollCatch;
