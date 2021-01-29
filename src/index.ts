bf6a736c81a85c8176e06af1df90ea96cd6d6b8a
Kartar Singh Jabanda <kartarsjabanda@gmail.com>
2021-01-29 10:26:03 +0000

    states working just clashing error on upscroll

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { isElmScrolledBottom, resetStyle } from './utils'
+import { isElmScrolledBottom, resetStyle, STATES, setActiveState } from './utils'
 
 // TODO: debounce
 
@@ -12,10 +12,11 @@ let scrollDirection: boolean
 
 window.addEventListener('scroll', () => {
   if (isElmScrolledBottom(target)) {
-    target.classList.add('sticky-scroll-catch--scrolled')
+    setActiveState(STATES.SCROLL_DOWN_CATCH, target)
   }
+
   if (isElmScrolledBottom(parent)) {
-    target.classList.add('sticky-scroll-catch--break')
+    setActiveState(STATES.SCROLL_BREAK, target)
   }
 
   // handle scroll up and 'catch' so scrolls with window
@@ -24,25 +25,17 @@ window.addEventListener('scroll', () => {
     let topDistanceFromParent: number = target.getBoundingClientRect().top - parent.getBoundingClientRect().top
     let bottomYAxisSpace: number = YAxisSpace - topDistanceFromParent
 
-    target.classList.remove('sticky-scroll-catch--scrolled')
-    target.classList.remove('sticky-scroll-catch--break')
-
+    setActiveState(STATES.UPSCROLL, target)
     target.style.bottom = `${bottomYAxisSpace}px`
-    target.classList.add('sticky-scroll-catch--upscroll')
 
     // fix to top
     if (target.getBoundingClientRect().top > 0) {
-      target.classList.remove('sticky-scroll-catch--scrolled')
-      target.classList.remove('sticky-scroll-catch--break')
-      target.classList.remove('sticky-scroll-catch--upscroll')
-      target.classList.add('sticky-scroll-catch--scrolled-top')
-
-      //   resetStyle(target, 'sticky-scroll-catch')
+      setActiveState(STATES.SCROLL_TOP, target)
     }
   }
 
-  if (!scrollDirection) {
-    target.classList.remove('sticky-scroll-catch--upscroll')
+  if (parent.getBoundingClientRect().top >= target.getBoundingClientRect().top) {
+    setActiveState(STATES.INITAL, target)
   }
 
   window.onscroll = function () {
@@ -51,3 +44,11 @@ window.addEventListener('scroll', () => {
     this.oldScroll = this.scrollY
   }
 })
+
+// for (let i = target.classList.length - 1; i >= 0; i--) {
+//     const className = target.classList[i]
+//     if (className.startsWith('sticky-scroll-catch-')) {
+//       // setActiveState(STATES.INITAL, target)
+//       console.log(className)
+//     }
+//   }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,7 @@ export const isElmScrolledBottom = (elm: HTMLElement): boolean => {
   return isScrolledToEnd
 }
 
-export const resetStyle = (elm: HTMLElement, classPrefix: string): void => {
+export const resetStyle = (elm: HTMLElement, classPrefix = 'sticky-scroll-catch'): void => {
   elm.style.bottom = null
   for (let i = elm.classList.length - 1; i >= 0; i--) {
     const className = elm.classList[i]
@@ -16,3 +16,31 @@ export const resetStyle = (elm: HTMLElement, classPrefix: string): void => {
     }
   }
 }
+
+// when one state is active all others are not
+interface StatesConfig {
+  INITAL: string
+  SCROLL_DOWN_CATCH: string
+  SCROLL_BREAK: string
+  UPSCROLL: string
+  SCROLL_TOP: string
+  SCROLL_TOP_BREAK: string
+}
+
+export const STATES: StatesConfig = {
+  INITAL: 'sticky-scroll-catch--initial',
+  SCROLL_DOWN_CATCH: 'sticky-scroll-catch--scrolled',
+  SCROLL_BREAK: 'sticky-scroll-catch--break',
+  UPSCROLL: 'sticky-scroll-catch--upscroll',
+  SCROLL_TOP: 'sticky-scroll-catch--scrolled-top',
+  SCROLL_TOP_BREAK: 'sticky-scroll-catch--initial', // (same as INITIAL, just there for understanding)
+}
+
+export const setActiveState = (classToAdd: string, elm: HTMLElement): void => {
+  resetStyle(elm)
+  elm.classList.add(classToAdd)
+}
+
+// export const resetState = (elm: HTMLElement): void => {
+//   setActiveState(STATES.INITAL, elm)
+// }
